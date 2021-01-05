aa51bec98f083e3444069c137914cac1da09b11c
unknown <felipenatale@gmail.com>
2021-01-05 19:17:01 -0300

    Paginacao implementada

--- app/config/routes.js
+++ app/config/routes.js
@@ -6,7 +6,7 @@ angular.module('primeiraApp').config([
             url: "/dashboard",
             templateUrl: "dashboard/dashboard.html"
         }).state('billingCycle', {
-            url: "/billingCycle",
+            url: "/billingCycle?page",
             templateUrl: "billingCycle/tabs.html"
         })
 
